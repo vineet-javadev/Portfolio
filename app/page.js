8e79b233198811a04dda45b9f0134fd47f4ecf0b
Vineet K. Chauhan <vineetkumar444777@gmail.com>
2025-05-18 17:35:00 +0530

    update

--- app/page.js
+++ app/page.js
@@ -131,7 +131,7 @@ export default function Home() {
 
       {/* background layout */}
       <div
-      className="fixed top-0 left-0 w-full min-h-screen bg-fixed bg-no-repeat bg-center transform scale-[0.7]"
+      className="fixed top-0 left-0 w-full min-full bg-fixed bg-no-repeat bg-center"
       style={{
         backgroundImage:
           "url('https://images.wallpaperscraft.com/image/single/moon_space_black_185222_3840x2160.jpg')",
